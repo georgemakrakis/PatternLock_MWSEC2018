77f5d2c45029e9087a72b83e1a2cd15691729948
Georgios Michail Makrakis <icsd13107@aegean.gr>
2018-03-16 20:52:20 +0200

    Button now only activates if username is not empty

--- app/src/main/java/com/example/georgemakrakis/patternlock/MainActivity.java
+++ app/src/main/java/com/example/georgemakrakis/patternlock/MainActivity.java
@@ -62,6 +62,7 @@ public class MainActivity extends AppCompatActivity
                 // TODO Auto-generated method stub
             }
         });
+
         start_stop_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)

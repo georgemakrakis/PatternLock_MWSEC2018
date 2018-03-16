482f32934140780aee9cf073a478daf5d1471985
georgemakrakis <georgemakrakis88@gmail.com>
2018-03-16 18:46:59 +0200

    Added first version of the pattern lock view

---
+++ app/src/main/java/com/example/georgemakrakis/patternlock/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.georgemakrakis.patternlock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+    private PatternLockView mPatternLockView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPatternLockView = (PatternLockView) findViewById(R.id.pattern_lock_view);
+        mPatternLockView.addPatternLockListener(mPatternLockViewListener);
+    }
+    private PatternLockViewListener mPatternLockViewListener = new PatternLockViewListener()
+    {
+        @Override
+        public void onStarted() {
+            Log.d(getClass().getName(), "Pattern drawing started");
+        }
+
+        @Override
+        public void onProgress(List<PatternLockView.Dot> progressPattern) {
+            Log.d(getClass().getName(), "Pattern progress: " +
+                    PatternLockUtils.patternToString(mPatternLockView, progressPattern));
+        }
+
+        @Override
+        public void onComplete(List<PatternLockView.Dot> pattern) {
+            Log.d(getClass().getName(), "Pattern complete: " +
+                    PatternLockUtils.patternToString(mPatternLockView, pattern));
+        }
+
+        @Override
+        public void onCleared() {
+            Log.d(getClass().getName(), "Pattern has been cleared");
+        }
+    };
+}

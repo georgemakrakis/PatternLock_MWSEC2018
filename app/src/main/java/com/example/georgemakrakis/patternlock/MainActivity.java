1383c4eae3c5f358d56286246095f06b70d62929
Georgios Michail Makrakis <icsd13107@aegean.gr>
2018-03-19 11:35:15 +0200

    Pattern logging, checks for wrong-duplicated patterns, at 10 repeat 3

--- app/src/main/java/com/example/georgemakrakis/patternlock/MainActivity.java
+++ app/src/main/java/com/example/georgemakrakis/patternlock/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.georgemakrakis.patternlock;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
@@ -13,13 +17,18 @@ import com.andrognito.patternlockview.PatternLockView;
 import com.andrognito.patternlockview.listener.PatternLockViewListener;
 import com.andrognito.patternlockview.utils.PatternLockUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
 {
     private PatternLockView mPatternLockView;
     private EditText username;
     private AppCompatButton start_stop_button;
+    private List<String> patternsList = new ArrayList<>();
+    private List<String> patternsList2 = new ArrayList<>();
+    private boolean flagAtThree = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -97,14 +106,63 @@ public class MainActivity extends AppCompatActivity
         @Override
         public void onComplete(List<PatternLockView.Dot> pattern)
         {
-            Log.d(getClass().getName(), "Pattern complete: " +
-                    PatternLockUtils.patternToString(mPatternLockView, pattern));
+            if(pattern.size()>=4)
+            {
+                String patternNow = PatternLockUtils.patternToString(mPatternLockView, pattern);
+                Log.d(getClass().getName(), "Pattern complete: " +
+                    patternNow);
+
+                if(flagAtThree)
+                {
+                    if(patternsList.contains(patternNow))
+                    {
+                        patternsList.add(patternNow);
+                    }
+                    else
+                    {
+                        //Log.d(getClass().getName(), "You have entered the same pattern twice, please enter a different pattern");
+                        ShowDialog("The pattern you entered is not one of the previous patterns");
+                    }
+                }
+                else
+                {
+                    if (!patternsList.contains(patternNow))
+                    {
+                        patternsList.add(patternNow);
+                    }
+                    else
+                    {
+                        //Log.d(getClass().getName(), "You have entered the same pattern twice, please enter a different pattern");
+                        ShowDialog("You have entered the same pattern twice, please enter a different pattern");
+                    }
+
+                    Log.d(getClass().getName(), "Patterns to list: " + patternsList.toString());
+                }
+            }
         }
 
         @Override
         public void onCleared()
         {
             Log.d(getClass().getName(), "Pattern has been cleared");
+            //When user reaches the 10 patterns input check if he remembers 3 of them
+            if(patternsList.size()==10)
+            {
+                ShowDialog("Now you must enter 3 of your previous patterns");
+                flagAtThree = true;
+            }
         }
     };
+    public void ShowDialog(String Message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(Message)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // do nothing
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 }
